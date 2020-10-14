50cce5b983570a64c5a9a5a574ae6241d3c092f9
Alan <Alan.Escudero.Caporal@gmail.com>
2020-10-14 18:29:36 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Fox
+          Alan es Furcia
         </p>
         <a
           className="App-link"
